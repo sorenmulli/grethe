b3568d89d43f0629ef87f99cc1c333fa836a1de3
sorenmulli <swholm@protonmail.com>
2021-01-17 16:47:03 +0100

    Board connection

--- bonnet.py
+++ bonnet.py
@@ -1,12 +1,12 @@
 LOADED = False
 try:
-    import aiy
+    from aiy.board import Board
     LOADED = True
 except ImportError:
     print("Could not load aiy.")
 
 def click_wait():
     if not LOADED: return 1
-    b = aiy.board.Button()
-    b.wait_for_release()
+    with Board() as board:
+        board.button.wait_for_release()
     return True

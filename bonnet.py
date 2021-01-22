af16849142660a79f51c11737599236ebbdaf9c5
Søren Winkel Holm <swholm@protonmail.com>
2021-01-22 20:27:32 +0100

    Add colours

--- bonnet.py
+++ bonnet.py
@@ -1,12 +1,31 @@
+from enum import Enum
+
 LOADED = False
 try:
     from aiy.board import Board
+    from aiy.leds import Leds
     LOADED = True
 except ImportError:
     print("Could not load aiy.")
 
+class Col(Enum):
+    """
+    Predefined colours for button.
+    """
+    BOOT    = (255, 153, 0)    # Orange
+    WAIT    = (102, 255, 204)  # Turqouis
+    WORKING = (102, 0, 255)    # Purple
+    ACTIVE  = (255, 102, 255)  # Pink
+
+
+
 def click_wait():
-    if not LOADED: return 1
+    if not LOADED: return
     with Board() as board:
         board.button.wait_for_release()
     return True
+
+def set_colour(colour: Col):
+    if not LOADED: return
+    with Leds() as leds:
+        leds.update(Leds.rgb_on(colour))
--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 from sayings import Sayings
 from text import TextDB
 from tts import speak
-from bonnet import click_wait
+from bonnet import click_wait, set_colour, Col
 
 
 def speak_status(textdb: TextDB, sayings: Sayings, lang: str):
+    set_colour(Col.ACTIVE)
     speak(sayings.time_greeting(), lang)
-
     speak(textdb.get_text("prestatus"), lang)
-    speak(sayings.coronastatus(), lang)
-    for saying in sayings.news():
+
+    set_colour(Col.WORKING)
+    corona = sayings.coronastatus()
+    set_colour(Col.ACTIVE)
+    speak(corona, lang)
+
+    set_colour(Col.WORKING)
+    news = sayings.news()
+    for saying in news:
+        set_colour(Col.ACTIVE)
         speak(saying, lang)
     speak(textdb.get_text("poststatus"), lang)
 
@@ -19,18 +27,24 @@ def main():
     textdb = TextDB(lang)
     sayings = Sayings(textdb)
 
+    set_colour(Col.ACTIVE)
+    speak(textdb.get_text("booted"), lang)
     while True:
+        set_colour(Col.WAIT)
         print("Ready! Waiting for press")
-        speak(textdb.get_text("booted"), lang)
-        if click_wait():
-            print("Clicked!")
-            try:
-                speak_status(textdb, sayings, lang)
-            except Exception as e:
-                print("ERROR")
-                print(e)
-                speak(textdb.get_text("speakerror"), lang)
+
+        # Code halts here
+        click_wait()
+        print("Clicked!")
+
+        try:
+            speak_status(textdb, sayings, lang)
+        except Exception as e:
+            print("ERROR")
+            print(e)
+            speak(textdb.get_text("speakerror"), lang)
 
 if __name__ == '__main__':
+    set_colour(Col.BOOT)
     print("Booting ...")
     main()

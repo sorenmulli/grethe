339e7d2bba85868b8169a5533d21be453132f7f6
Søren Winkel Holm <swholm@protonmail.com>
2021-01-22 19:40:45 +0100

    Cleanup + more sayings

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from tts import speak
 from bonnet import click_wait
 
 
+def speak_status(textdb: TextDB, sayings: Sayings, lang: str):
+    speak(sayings.time_greeting(), lang)
+
+    speak(textdb.get_text("prestatus"), lang)
+    speak(sayings.coronastatus(), lang)
+    for saying in sayings.news():
+        speak(saying, lang)
+    speak(textdb.get_text("poststatus"), lang)
+
 def main():
     lang = "da"
     textdb = TextDB(lang)
@@ -12,15 +21,15 @@ def main():
 
     while True:
         print("Ready! Waiting for press")
+        speak(textdb.get_text("booted"), lang)
         if click_wait():
             print("Clicked!")
-            speak(sayings.time_greeting(), lang)
-
-            speak(textdb.get_text("prestatus"), lang)
-            speak(sayings.coronastatus(), lang)
-            for saying in sayings.news():
-                speak(saying, lang)
-            speak(textdb.get_text("poststatus"), lang)
+            try:
+                speak_status(textdb, sayings, lang)
+            except Exception as e:
+                print("ERROR")
+                print(e)
+                speak(textdb.get_text("speakerror"), lang)
 
 if __name__ == '__main__':
     print("Booting ...")
